a91283cc4063d14266c03721040a2e5a1219a60f
Sutrishna Bhunia <134145442+sutrishnabhunia@users.noreply.github.com>
2023-07-19 12:22:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-//your JS code here. If required.
-function show(x,y)
-{
-    let count=0;
-    const t=setInterval(()=>{
-        const a=new Date();
-        const b=a.toLocaleString();
-        document.getElementById('timer').innerHTML = b;
-        count++;
-        if(count===y)
-            clearInterval(t)
-    },x*1000)
-}
-show(1,10)
+//your JS code here. If required
+let p=document.createElement("p");
+p.setAttribute('id','timer')
+document.body.append(p);
+setInterval(function(){
+ let date=new Date();
+ let an=date.getMonth()+"/"+date.getDate()+"/"+date.getFullYear()+", "+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+// console.log(typeof date.getHours());
+ if(date.getHours()>=12 && date.getHours()<=23)
+ {
+    an=an+" PM";
+ }
+ else{
+an=an+" AM"
+ }
+ p.innerHTML=an;
+},1000);.
