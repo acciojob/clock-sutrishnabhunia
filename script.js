e3d0b0adf9d2c2751a697b48d9f368377dda3acc
Sutrishna Bhunia <134145442+sutrishnabhunia@users.noreply.github.com>
2023-07-19 12:19:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+//your JS code here. If required
+updateclock();
+function updateclock() {
+	const currTime = new Date();
+	const hours = currTime.getHours();
+	const minutes = currTime.getMinutes();
+	const seconds = currTime.getSeconds();
+
+
+	 minutes = (minutes < 10 ? "0" : "") + minutes;
+        seconds = (seconds < 10 ? "0" : "") + seconds;
+
+        // Determine AM/PM
+        var period = hours < 12 ? "AM" : "PM";
+
+        // Convert to 12-hour format
+        hours = hours > 12 ? hours - 12 : hours;
+        hours = hours === 0 ? 12 : hours;
+
+	
+	var clockDiv = document.getElementById("clock");
+	clockDiv.textContent = hours + ":" + minutes + ":" + seconds + " " + period;
+}
+	export default updateclock();
+setInterval(updateClock, 1000);
